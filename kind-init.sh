3b6b929839a09b290434a1f9ce6fdddafa748454
Kacper Siuda <kacper.siuda@fitatu.com>
2022-03-01 22:14:25 +0100

    Update

--- kind-init.sh
+++ kind-init.sh
@@ -6,7 +6,12 @@ kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 nodes:
 - role: control-plane
-- role: worker
+  kubeadmConfigPatches:
+  - |
+    kind: InitConfiguration
+    nodeRegistration:
+      kubeletExtraArgs:
+        node-labels: "ingress-ready=true"
   extraPortMappings:
   - containerPort: 80
     hostPort: 80
@@ -14,10 +19,4 @@ nodes:
   - containerPort: 443
     hostPort: 443
     protocol: TCP
-  kubeadmConfigPatches:
-  - |
-    kind: JoinConfiguration
-    nodeRegistration:
-      kubeletExtraArgs:
-        node-labels: "ingress-ready=true"
 EOF
